22ca8a943c5f34b725782fa0f8a7a599282fb16c
jang <dkanvk1@gmail.com>
2022-06-10 17:38:50 +0900

    22.6.10

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import {join} from "../controllers/userController";
-import { trending } from "../controllers/vidoeController";
+import { join, login } from "../controllers/userController";
+import { trending, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
 // import한 controller 사용
 globalRouter.get("/", trending);
-globalRouter.get("/join",join);
+globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { watch, edit } from "../controllers/vidoeController";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id/edit(\\d+)", edit);
+videoRouter.get("/:id/delete(\\d+)", deleteVideo);
 
 export default videoRouter;
 // 파일의 일부만 export 할 수 있도록 익스포트함
