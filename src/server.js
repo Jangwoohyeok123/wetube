4483126a99ea10da41327dde83a2bf2e39363786
jang <dkanvk1@gmail.com>
2022-06-11 22:50:26 +0900

    22.6.11

--- src/server.js
+++ src/server.js
@@ -4,13 +4,13 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-
 const PORT = 4000;
-
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views"); // pug가 봐야할 파일의 경로를 바꾸는 함수 
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
