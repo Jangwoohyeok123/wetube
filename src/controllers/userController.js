2ebd1acae7e34d678626bd325f74548a2e4bc41e
jang <dkanvk1@gmail.com>
2022-06-19 23:45:40 +0900

    22.6.19

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,6 @@
-import res from "express/lib/response";
-
 export const join = (req, res) => res.send("Join");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("login");
-export const logout = (req, res) => res.send("logout");
-export const see = (req,res) => res.send("See User");
-
+export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Log out");
+export const see = (req, res) => res.send("See User");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,17 @@
 import express from "express";
 import {
   watch,
+  getUpload,
   getEdit,
   postEdit,
-  getUpload,
-  postUpload
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
+
 export default videoRouter;
 // 파일의 일부만 export 할 수 있도록 익스포트함
